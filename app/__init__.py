a0743afb2cf2df4c0e46262160265a6dc8433c03
Jisha Obukwelu <jisha@obukwelu.com>
2020-08-22 15:02:44 -0400

    Add mongo package

--- app/__init__.py
+++ app/__init__.py
@@ -4,19 +4,20 @@ from flask  import Flask
 
 from app.routes.routes import routes
 
-# load_dotenv()
 
-# DATABASE_URL = os.getenv("DATABASE_URL")
-# SECRET_KEY = os.getenv("SECRET_KEY")
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 def create_app():
     APP = Flask(__name__)
-    # APP.config["SECRET_KEY"] = SECRET_KEY
+    APP.config["SECRET_KEY"] = SECRET_KEY
 
-    # APP.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
-    # APP.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    # DB.init_app(APP)
-    # migrate.init_app(APP, DB)
+    APP.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
+    APP.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    DB.init_app(APP)
+    migrate.init_app(APP, DB)
 
     APP.register_blueprint(routes)
 
