f85ee5b5957de04600a53bddf5ac9b81fc03462f
Jisha Obukwelu <jisha@obukwelu.com>
2020-08-24 21:41:13 -0400

    app organization

---
+++ app/routes/main.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+from  app.model import PredictionBot
+
+API = Flask(__name__)
+
+@API.route('/')
+def index():
+    return jsonify("App Online")
+
+@API.route('/search/<user_input>')
+def search(user_input):
+    bot = PredictionBot()
+    return jsonify(bot.predict(user_input))
+
+
+if __name__ == '__main__':
+    API.run(debug = True)
