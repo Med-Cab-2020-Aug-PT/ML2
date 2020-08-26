e9bd2c1f55193a86fae0cd2dddb3fee7db9c8b44
Jisha Obukwelu <jisha@obukwelu.com>
2020-08-25 22:13:35 -0400

    Update .env file

--- app/model.py
+++ app/model.py
@@ -19,11 +19,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# DB_USER = os.getenv("MONGO_USER", default="OOPS")
-# DB_PASSWORD = os.getenv("MONGO_PASSWORD", default="OOPS")
-# DB_URI = os.getenv("MONGO_URI", default="OOPS")
+# DB_USER = getenv("MONGO_USER", default="OOPS")
+# DB_PASSWORD = getenv("MONGO_PASSWORD", default="OOPS")
+# DB_URI = getenv("MONGO_URI", default="OOPS")
+DB_URL = getenv("MONGO_URL", default="OOPS")
 
-client = MongoClient(f"{MONGO_URL}")
+client = MongoClient(f"mongodb+srv://{DB_USER}:{DB_PASSWORD}@{DB_URI}/test?retryWrites=true&w=majority")
+# client = MongoClient(f"{DB_URL}")
 
 FILEPATH =  os.path.join(os.path.dirname(__file__),'data', 'csv', 'cannabis.csv')
 
