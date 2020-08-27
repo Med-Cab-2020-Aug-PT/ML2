192880a4300829bb7b0a06d32a4acc391e24feed
Jisha Obukwelu <jisha@obukwelu.com>
2020-08-26 21:21:45 -0400

    Update cosine and nn models' tokens

--- app/model.py
+++ app/model.py
@@ -6,9 +6,10 @@ Input  --> TF-IDF -->  Cosine_Similarity --> Output
 Input  --> TF-IDF -->  Nearest Neighbor --> Output
 """
 
-import pandas as pd
 import os
 from os import getenv
+import pandas as pd
+import pickle
 
 from pymongo import MongoClient
 
@@ -21,28 +22,34 @@ from dotenv import load_dotenv
 load_dotenv()
 
 FILEPATH =  os.path.join(os.path.dirname(__file__),'data', 'csv', 'cannabis.csv')
+DTM_FILEPATH =  os.path.join(os.path.dirname(__file__),'data', 'pickled_models', 'dtm.pkl')
+TFIDF_FILEPATH =  os.path.join(os.path.dirname(__file__),'data', 'pickled_models', 'tfidf.pkl')
 
 class PredictionBot:
     """NLP Bot for Cannabis Suggestion App"""
 
+
     db =  MongoClient(f"{getenv('MONGO_URL')}").medcabinet.strains
     df = pd.read_csv(FILEPATH)
 
-    tfidf = TfidfVectorizer()
-    nn = NearestNeighbors(n_neighbors=1, n_jobs=1)
-    tokens = tfidf.fit_transform(
-        df['Description']
-    )
-    nearest = nn.fit(
-        pd.DataFrame(tokens.todense(), columns=tfidf.get_feature_names())
-    )
+    #Pickled models
+    tfidf_model = pickle.load(open(DTM_FILEPATH, 'rb'))
+    dtm_model = pickle.load(open(TFIDF_FILEPATH, 'rb'))
 
-    def predict(self, user_input):
-        return next(self.db.find({'_id': int(self.nearest.kneighbors(
-            self.tfidf.transform([user_input]).todense()
-            )[1][0][0])}))
+    # def predict(self, user_input):
+    #     return next(self.db.find({'_id': int(self.nearest.kneighbors(
+    #         self.tfidf.transform([user_input]).todense()
+    #         )[1][0][0])}))
+
+    def cosine_recommender(self, user_input):
+        user_dtm = pd.DataFrame(self.tfidf_model.transform(user_input).todense(), columns=tfidf_model.get_feature_names())
+        rec_dtm = self.dtm_model.append(user_dtm1).reset_index(drop=True)
+        cosine_df = pd.DataFrame(cosine_similarity(rec_dtm1))
+
+        recommendations5 = next(self.db.find({'_id': int(cosine_df1[cosine_df1[0] < 1][0].sort_values(ascending=False)[:5])[1][0][0]}))
+        return recommendations5
 
 if __name__ == "__main__":
     bot = PredictionBot()
-    print(bot.predict("Some text in here.. "))
+    print(bot.cosine_recommender("Some text in here.. "))
 
