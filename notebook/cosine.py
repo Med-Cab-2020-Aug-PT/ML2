9f03b3e5f937155569ce713f583d00b80b62e13a
Jisha Obukwelu <jisha@obukwelu.com>
2020-08-27 17:30:30 -0400

    Capture the index of cosine_similarity strains

--- notebook/cosine.py
+++ notebook/cosine.py
@@ -21,44 +21,44 @@ df = pd.read_csv('app/data/csv/cannabis.csv')
 print('Shape:', df.shape)
 print(df.head())
 
-## spaCy
-TOKENIZE_FILEPATH =  'app/data/pickled_models/tokenize.pkl'
+# ## spaCy
+# TOKENIZE_FILEPATH =  'app/data/pickled_models/tokenize.pkl'
 
-#Loading the pickled models
-tokenize = pickle.load(open(TOKENIZE_FILEPATH, 'rb'))
-print(tokenize)
+# #Loading the pickled models
+# tokenize = pickle.load(open(TOKENIZE_FILEPATH, 'rb'))
+# print(tokenize)
 
-# Instantiate vectorizer object
-tfidf = TfidfVectorizer(stop_words='english', 
-                        ngram_range=(1,3),
-                        max_df=.97,
-                        min_df=3,
-                        tokenizer=tokenize,
-                        max_features = 5000)
+# # Instantiate vectorizer object
+# tfidf = TfidfVectorizer(stop_words='english', 
+#                         ngram_range=(1,3),
+#                         max_df=.97,
+#                         min_df=3,
+#                         tokenizer=tokenize,
+#                         max_features = 5000)
 
-# Create a vocabulary and get word counts per document
-dtm = tfidf.fit_transform(df['Type'] + df['Description'] + df['Effects'] + df['Flavors']) # Similiar to fit_predict
+# # Create a vocabulary and get word counts per document
+# dtm = tfidf.fit_transform(df['Type'] + df['Description'] + df['Effects'] + df['Flavors']) # Similiar to fit_predict
 
-# Get feature names to use as dataframe column headers
-dtm = pd.DataFrame(dtm.todense(), columns=tfidf.get_feature_names())
+# # Get feature names to use as dataframe column headers
+# dtm = pd.DataFrame(dtm.todense(), columns=tfidf.get_feature_names())
 
-# View Feature Matrix as DataFrame
-print(dtm.head())
+# # View Feature Matrix as DataFrame
+# print(dtm.head())
 
-# Create cosine_similarity function
-import json
+# # Create cosine_similarity function
+# import json
 
-def cosine_recommender(user_input):
+# def cosine_recommender(user_input):
 
-    user_dtm = pd.DataFrame(tfidf.transform(user_input).todense(), columns=tfidf.get_feature_names())
-    rec_dtm = dtm.append(user_dtm).reset_index(drop=True)
-    cosine_df = pd.DataFrame(cosine_similarity(rec_dtm))
-    recommendations = cosine_df[cosine_df[0] < 1][0].sort_values(ascending=False)[:5]
+#     user_dtm = pd.DataFrame(tfidf.transform(user_input).todense(), columns=tfidf.get_feature_names())
+#     rec_dtm = dtm.append(user_dtm).reset_index(drop=True)
+#     cosine_df = pd.DataFrame(cosine_similarity(rec_dtm))
+#     recommendations = cosine_df[cosine_df[0] < 1][0].sort_values(ascending=False)[:5]
 
-    return recommendations
+#     return recommendations
 
-user_input1 =["I am feeling sluggish. I am looking for an ammonia flavored strain that will have me feeling happy and energetic"]
-print(cosine_recommender(user_input1))
+user_input =["I am feeling sluggish. I am looking for an ammonia flavored strain that will have me feeling happy and energetic"]
+# print(cosine_recommender(user_input1))
 
 # pickle new model
 # TFIDF_FILEPATH = os.path.join(os.path.dirname(__file__), "..","app", "data", "pickled_models", "tfidf.pkl")
@@ -91,8 +91,7 @@ def cosine_recs(user_input):
     Function takes user input and transforms into vectorized dataframe
     This is then added to the dtm 
     Then it  calculates the similarity between input and strain 
-    and returns the top 5 closest matches. 
-
+    and returns the id of the top 5 closest matches.  
     '''
     #Vectorizes user input
     user_dtm1 = pd.DataFrame(tfidf_model.transform(user_input).todense(), columns=tfidf_model.get_feature_names())
@@ -106,10 +105,11 @@ def cosine_recs(user_input):
     #Finds 5 closet strains.
     recommendations5 = cosine_df1[cosine_df1[0] < 1][0].sort_values(ascending=False)[:5]
 
-    rec_result = recommendations5.index
+    #Just grab index numbers
+    rec_result = recommendations5.index.tolist()
 
     return rec_result
 
-print(cosine_recs(user_input1))
+print(cosine_recs(user_input))
 
 #have to create separate 
