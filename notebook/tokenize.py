2d8dad92eb435d0541fbc747100acf975194442b
Jisha Obukwelu <jisha@obukwelu.com>
2020-08-26 23:14:55 -0400

    Working on pickling tokenize function

---
+++ notebook/tokenize.py
@@ -0,0 +1,36 @@
+"""Import Statements"""
+import os
+import pickle
+import re
+from spacy.lang.en import English
+from spacy.lang.en.stop_words import STOP_WORDS
+
+nlp = English()
+
+STOP_WORDS = nlp.Defaults.stop_words.union({"$", '-', '', ' ',
+                                            'bred','breed', 'breeds','call', 'calls',
+                                            'combine', 'combines','consumer', 'contains','containing',
+                                            "don't", 'effect', 'effects','especially','explanations',
+                                            'flavor', 'flavors','flower','give', 'gives','got',  'high',
+                                            'i', "i'm", "i've",'including','it.', "it's",
+                                            'like', 'match', 'matches','making',
+                                            'offer', 'offers','pack', 'packs','price', 'probably',
+                                            'produce', 'produces', 'really',
+                                            'refers', 'report', 'reports', "'s", 's',
+                                            'seed', 'seeds','showing',
+                                            'smell','start', 'started', 'stem', 'stems',
+                                            'strain', 'strains','supposedly',
+                                            'technique', 'techniques','tend', 'tends',
+                                            'unavailable', 'unkown', 'user', 'users', 'utlizing',
+                                            'weed', 'week', None})
+
+def tokenize(text):
+  doc = nlp(text) #casting as text
+  return [token.lemma_.strip() for token in doc if not token.is_stop and not token.is_punct]
+
+MODEL_FILEPATH = os.path.join(os.path.dirname(__file__), "tokenize.pkl")
+
+with open(MODEL_FILEPATH, "wb") as model_file:
+  print("SAVE PICKLE 1")
+  pickle.dump(tokenize, model_file, protocol=pickle.HIGHEST_PROTOCOL)
+
