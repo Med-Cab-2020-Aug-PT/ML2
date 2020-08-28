da35db8f637a399bc5d0a41f33343c5697da09cb
Jisha Obukwelu <jisha@obukwelu.com>
2020-08-27 23:49:46 -0400

    Update pickle files to use small package

--- notebook/cosine.py
+++ notebook/cosine.py
@@ -13,17 +13,47 @@ from spacy.tokenizer import Tokenizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
+# Import cannabis data
 df = pd.read_csv('app/data/csv/cannabis.csv')
 
 print('Shape:', df.shape)
 print(df.head())
 
-## spaCy
+# spaCy
+nlp = spacy.load("en_core_web_md")
+
+STOP_WORDS = nlp.Defaults.stop_words.union({"$", '-', '', ' ',
+                                            'bred','breed', 'breeds','call', 'calls',
+                                            'combine', 'combines','consumer', 'contains','containing',
+                                            "don't", 'effect', 'effects','especially','explanations',
+                                            'flavor', 'flavors','flower','give', 'gives','got',  'high',
+                                            'i', "i'm", "i've",'including','it.', "it's",
+                                            'like', 'match', 'matches','making',
+                                            'offer', 'offers','pack', 'packs','price', 'probably',
+                                            'produce', 'produces', 'really',
+                                            'refers', 'report', 'reports', "'s", 's',
+                                            'seed', 'seeds','showing',
+                                            'smell','start', 'started', 'stem', 'stems',
+                                            'strain', 'strains','supposedly',
+                                            'technique', 'techniques','tend', 'tends',
+                                            'unavailable', 'unkown', 'user', 'users', 'utlizing',
+                                            'weed', 'week', None})
+
+def tokenize(text):
+  doc = nlp(text) #casting as text
+  return [token.lemma_.strip() for token in doc if not token.is_stop and not token.is_punct]
+
+# Pickle tokenizer model
+TOKENIZE_FILEPATH = os.path.join(os.path.dirname(__file__),"..", "app", "data", "pickled_models", "tokenize.pkl")
+pickle.dump(tokenize, open(TOKENIZE_FILEPATH, 'wb'))
+print("save 1")
+
+#Open pickled tokenizer for tfidf model
 TOKENIZE_FILEPATH =  'app/data/pickled_models/tokenize.pkl'
 
 #Loading the pickled models
 tokenize = pickle.load(open(TOKENIZE_FILEPATH, 'rb'))
-print(tokenize)
+print("Open Pickle")
 
 # Instantiate vectorizer object
 tfidf = TfidfVectorizer(stop_words='english', 
@@ -55,29 +85,29 @@ def cosine_recommender(user_input):
     return recommendations
 
 user_input =["I am feeling sluggish. I am looking for an ammonia flavored strain that will have me feeling happy and energetic"]
+print(cosine_recommender(user_input))
 
-# pickle new model
+# pickle TFIDF
 TFIDF_FILEPATH = os.path.join(os.path.dirname(__file__), "..","app", "data", "pickled_models", "tfidf.pkl")
+pickle.dump(tfidf, open(TFIDF_FILEPATH, 'wb'))
+print("save 2")
 
-with open(TFIDF_FILEPATH, "wb") as model_file:
-  print("SAVE PICKLE 2")
-  pickle.dump(tfidf, model_file, protocol=pickle.HIGHEST_PROTOCOL)
-
+# pickle DTM
 DTM_FILEPATH = os.path.join(os.path.dirname(__file__), "..","app", "data", "pickled_models", "dtm.pkl")
+pickle.dump(dtm, open(DTM_FILEPATH, 'wb'))
+print("save 2")
 
-with open(DTM_FILEPATH, "wb") as model_file:
-  print("SAVE PICKLE 3")
-  pickle.dump(dtm, model_file, protocol=pickle.HIGHEST_PROTOCOL)
 
+#Testing function with pickled models
+user_input =["I am feeling sluggish. I am looking for an ammonia flavored strain that will have me feeling happy and energetic"]
 
 #Testing model
-
-DTM_FILEPATH =  'app/data/pickled_models/dtm.pkl'
-TFIDF_FILEPATH = 'app/data/pickled_models/tfidf.pkl'
+DTM_OPEN_FILEPATH =  'app/data/pickled_models/dtm.pkl'
+TFIDF_OPEN_FILEPATH = 'app/data/pickled_models/tfidf.pkl'
 
 #Loading the pickled models
-tfidf_model = pickle.load(open(TFIDF_FILEPATH, 'rb'))
-dtm_model = pickle.load(open(DTM_FILEPATH, 'rb'))
+dtm_model = pickle.load(open(DTM_OPEN_FILEPATH, 'rb'))
+tfidf_model = pickle.load(open(TFIDF_OPEN_FILEPATH, 'rb'))
 
 print(tfidf_model)
 print(dtm_model)
@@ -102,7 +132,7 @@ def cosine_recs(user_input):
     recommendations5 = cosine_df1[cosine_df1[0] < 1][0].sort_values(ascending=False)[:5]
 
     #Just grab index numbers
-    rec_result = recommendations5.index.list()
+    rec_result = recommendations5.index.tolist()
 
     return rec_result
 
